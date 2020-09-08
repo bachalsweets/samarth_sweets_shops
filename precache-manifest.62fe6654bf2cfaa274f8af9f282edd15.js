3b7b822132cbf38511aad963ab8123e3183e93bf
somnathbachal023@gmail.com <somnathbachal023@gmail.com>
2020-09-08 19:47:18 +0530

    Updates

---
+++ precache-manifest.62fe6654bf2cfaa274f8af9f282edd15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a551804f706bc2c42ec7a0ffe44d807",
+    "url": "/samarth_sweets_shops/index.html"
+  },
+  {
+    "revision": "f996d506ef409d7d7a0e",
+    "url": "/samarth_sweets_shops/static/css/main.b4fa44ea.chunk.css"
+  },
+  {
+    "revision": "717f57e616f7860520d4",
+    "url": "/samarth_sweets_shops/static/js/2.c53530ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/samarth_sweets_shops/static/js/2.c53530ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f996d506ef409d7d7a0e",
+    "url": "/samarth_sweets_shops/static/js/main.36a22f49.chunk.js"
+  },
+  {
+    "revision": "efc5b0cd1469af3f11e9",
+    "url": "/samarth_sweets_shops/static/js/runtime-main.f2e97582.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/samarth_sweets_shops/static/media/logo.5d5d9eef.svg"
+  }
+]);
